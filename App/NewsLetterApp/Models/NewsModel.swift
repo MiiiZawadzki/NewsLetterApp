9fb094fb55db4027120995a88d416142d2f8cb33
Michal Zawadzki <zawadzki.micha@gmail.com>
2020-12-30 16:30:55 +0100

    Code cleaning

--- App/NewsLetterApp/Models/NewsModel.swift
+++ App/NewsLetterApp/Models/NewsModel.swift
@@ -3,18 +3,6 @@ import Foundation
 struct NewsModel: Codable{
     let status: String
     var articles = [News]()
-    func ConvertModelToDict() -> [[String:String]] {
-        var resultDict = [[String:String]]()
-        for news in self.articles{
-            var tmpDict = ["1date":"","2title":"","3summary":"","4link":""]
-            tmpDict.updateValue(news.published_date, forKey: "1date")
-            tmpDict.updateValue(news.title, forKey: "2title")
-            tmpDict.updateValue(news.summary, forKey: "3summary")
-            tmpDict.updateValue("[ "+news.clean_url+" ]", forKey: "4link")
-            resultDict.append(tmpDict)
-        }
-        return resultDict
-    }
 }
 
 struct News: Codable {
